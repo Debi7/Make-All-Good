d2b48326ca89ea5318fa076acaff4cfcc949c6c5
Debi7 <debi1@mail.ru>
2024-03-18 02:10:08 +0300

    eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -2,6 +2,6 @@
 
 module.exports = {
   env: { browser: true, es2020: true },
-  extends: ['eslint:recommended'],
+  extends: 'eslint:recommended',
   parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
 };
